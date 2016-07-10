3fbbc6d4c39aadc044f6dbf7e5bbd8cf76172b11
Kosinkadink <kosinkadink1@gmail.com>
2016-07-10 15:48:25 -0500

    made template for evaluator

---
+++ CS303_Project2a/CS303_Project2a/Evaluator.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+#include <string>
+#include <sstream>
+#include <stack>
+#include "EvaluatorError.h"
+
+using namespace std;
+
+class Evaluator
+{
+public:
+	Evaluator() {}
+	int eval(string expression);
+private:
+
+};
+
+
+int eval(string expression)
+{
+
+}
--- CS303_Project2a/CS303_Project2a/main.cpp
+++ CS303_Project2a/CS303_Project2a/main.cpp
@@ -1,4 +1,4 @@
-
+#include "Evaluator.h"
 
 int main()
 {
